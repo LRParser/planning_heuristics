4753db044e5c5593e04df7b45e2546352f6c575d
LRParser <jth82@drexel.edu>
2017-07-01 19:38:35 +0000

    Updating search script

--- run_all_searches.sh
+++ run_all_searches.sh
@@ -2,28 +2,28 @@
 
 echo "Basic comparison"
 
-python run_search.py -p 1 -s 1
-python run_search.py -p 1 -s 3
-python run_search.py -p 1 -s 8
+pypy3 run_search.py -p 1 -s 1
+pypy3 run_search.py -p 1 -s 3
+pypy3 run_search.py -p 1 -s 8
 
-python run_search.py -p 2 -s 1
-python run_search.py -p 2 -s 3
-python run_search.py -p 2 -s 8
+pypy3 run_search.py -p 2 -s 1
+pypy3 run_search.py -p 2 -s 3
+pypy3 run_search.py -p 2 -s 8
 
-python run_search.py -p 3 -s 1
-python run_search.py -p 3 -s 3
-python run_search.py -p 3 -s 8
+pypy3 run_search.py -p 3 -s 1
+pypy3 run_search.py -p 3 -s 3
+pypy3 run_search.py -p 3 -s 8
 
 echo "A star comparison"
 
-python run_search.py -p 1 -s 8
-python run_search.py -p 1 -s 9
-python run_search.py -p 1 -s 10
+pypy3 run_search.py -p 1 -s 8
+pypy3 run_search.py -p 1 -s 9
+pypy3 run_search.py -p 1 -s 10
 
-python run_search.py -p 2 -s 8
-python run_search.py -p 2 -s 9
-python run_search.py -p 2 -s 10
+pypy3 run_search.py -p 2 -s 8
+pypy3 run_search.py -p 2 -s 9
+pypy3 run_search.py -p 2 -s 10
 
-python run_search.py -p 3 -s 8
-python run_search.py -p 3 -s 9
-python run_search.py -p 3 -s 10
+pypy3 run_search.py -p 3 -s 8
+pypy3 run_search.py -p 3 -s 9
+pypy3 run_search.py -p 3 -s 10
